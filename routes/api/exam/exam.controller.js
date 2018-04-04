f0dcac35dd425f81efb8f4d67522cfa0fc22f345
Yoon Jee Woo <yoonjeewoo@gmail.com>
2018-04-04 18:12:23 +0900

    methods

--- routes/api/exam/exam.controller.js
+++ routes/api/exam/exam.controller.js
@@ -4,12 +4,12 @@ const config = require('../../../config');
 const conn = mysql.createConnection(config);
 
 exports.createExam = (req, res) => {
-  const { title, school, grade, semester, question_num } = req.body;
+  const { title, school, grade, semester, question_num, school_index, grade_index, semester_index } = req.body;
   const d = new Date();
   d.setUTCHours(d.getUTCHours() + 9);
   conn.query(
-    `INSERT INTO Exam(title, school, grade, semester, question_num, created_at) VALUES(?, ?, ?, ?, ?, ?)`,
-    [title, school, grade, semester, question_num, d],
+    `INSERT INTO Exam(title, school, grade, semester, question_num, created_at, school_index, grade_index, semester_index) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?)`,
+    [title, school, grade, semester, question_num, d, school_index, grade_index, semester_index],
     (err, result) => {
       if(err) throw err;
       return res.status(200).json({
@@ -20,10 +20,10 @@ exports.createExam = (req, res) => {
 }
 
 exports.createProblem = (req, res) => {
-  const { problem_num, small, activity, level, exam_id, accuracy, content, big } = req.body;
+  const { problem_num, small, activity, level, exam_id, accuracy, content, big, big_index, small_index } = req.body;
   conn.query(
-    `INSERT INTO Problem(problem_num, small, activity, level, exam_id, accuracy, content, big) VALUES(?, ?, ?, ?, ?, ?, ?, ?)`,
-    [problem_num, small, activity, level, exam_id, accuracy, content, big],
+    `INSERT INTO Problem(problem_num, small, activity, level, exam_id, accuracy, content, big, big_index, small_index) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`,
+    [problem_num, small, activity, level, exam_id, accuracy, content, big, big_index, small_index],
     (err, result) => {
       if (err) throw err;
       return res.status(200).json({
@@ -62,8 +62,8 @@ exports.getProblemList = (req, res) => {
 exports.updateProblem = (req, res) => {
   const { problem_num, small, activity, level, exam_id, accuracy, content, big } = req.body;
   conn.query(
-    'UPDATE Problem SET problem_num = ?, small = ?, activity = ?, level = ?, exam_id = ?, accuracy = ?, content = ?, big = ? WHERE exam_id = ? and problem_num = ?',
-    [problem_num, small, activity, level, exam_id, accuracy, content, big, exam_id, problem_num],
+    'UPDATE Problem SET problem_num = ?, small = ?, activity = ?, level = ?, exam_id = ?, accuracy = ?, content = ?, big = ?, big_index = ?, small_index = ? WHERE exam_id = ? and problem_num = ?',
+    [problem_num, small, activity, level, exam_id, accuracy, content, big, big_index, small_index, exam_id, problem_num],
     (err, result) => {
       if (err) throw err;
       return res.status(200).json({
