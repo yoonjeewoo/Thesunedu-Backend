925b26f5aee0db6651fd71d8ede9832c260f7011
Yoon Jee Woo <yoonjeewoo@gmail.com>
2018-04-17 19:05:55 +0900

    exam/result

--- routes/api/exam/exam.controller.js
+++ routes/api/exam/exam.controller.js
@@ -3,119 +3,134 @@ const mysql = require('mysql');
 const config = require('../../../config');
 const conn = mysql.createConnection(config);
 
+exports.getResult = (req, res) => {
+	console.log('asdf');
+	conn.query(
+		'SELECT exam_id, R.problem_num, activity, small, level, result, accuracy, student_name FROM (SELECT exam_id, problem_num, activity, small, `level`, accuracy FROM Exam join Problem on Exam.id = Problem.exam_id WHERE Exam.id = ? ORDER BY problem_num) as P join Result as R on P.exam_id = R.test_id WHERE student_name = ? and P.problem_num = R.problem_num;',
+		[req.query.exam_id, req.query.student_name],
+		(err, result) => {
+			if (err) throw err;
+	    return res.status(200).json({
+	    	result
+	    })
+		}
+	)
+}
 exports.createExam = (req, res) => {
   const { title, school, grade, semester, question_num, school_index, grade_index, semester_index, writer} = req.body;
   const d = new Date();
   d.setUTCHours(d.getUTCHours() + 9);
   conn.query(
-    `INSERT INTO Exam(title, school, grade, semester, question_num, created_at, school_index, grade_index, semester_index, writer) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`,
-    [title, school, grade, semester, question_num, d, school_index, grade_index, semester_index, writer],
-    (err, result) => {
-      if(err) throw err;
-      return res.status(200).json({
-        insert_id: result.insertId
-      })
-    }
+	`INSERT INTO Exam(title, school, grade, semester, question_num, created_at, school_index, grade_index, semester_index, writer) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`,
+	[title, school, grade, semester, question_num, d, school_index, grade_index, semester_index, writer],
+	(err, result) => {
+	  if(err) throw err;
+	  return res.status(200).json({
+		insert_id: result.insertId
+	  })
+	}
   )
 }
 
 exports.createProblem = (req, res) => {
   const { problem_num, small, activity, level, exam_id, accuracy, content, big, big_index, small_index } = req.body;
   conn.query(
-    `INSERT INTO Problem(problem_num, small, activity, level, exam_id, accuracy, content, big, big_index, small_index) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`,
-    [problem_num, small, activity, level, exam_id, accuracy, content, big, big_index, small_index],
-    (err, result) => {
-      if (err) throw err;
-      return res.status(200).json({
-        message: 'success'
-      })
-    }
+	`INSERT INTO Problem(problem_num, small, activity, level, exam_id, accuracy, content, big, big_index, small_index) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`,
+	[problem_num, small, activity, level, exam_id, accuracy, content, big, big_index, small_index],
+	(err, result) => {
+	  if (err) throw err;
+	  return res.status(200).json({
+		message: 'success'
+	  })
+	}
   )
 }
 
 exports.getExamList = (req, res) => {
   conn.query(
-    'SELECT count(P.id) as current_count, E.id, E.title, E.created_at, E.question_num, E.school, E.grade, E.writer FROM Exam as E left join Problem as P on P.exam_id = E.id Group by E.id;',
-    (err, result) => {
-      if (err) throw err;
-      return res.status(200).json({
-        result
-      })
-    }
+	'SELECT count(P.id) as current_count, E.id, E.title, E.created_at, E.question_num, E.school, E.grade, E.writer FROM Exam as E left join Problem as P on P.exam_id = E.id Group by E.id;',
+	(err, result) => {
+	  if (err) throw err;
+	  return res.status(200).json({
+		result
+	  })
+	}
   )
 }
 
 exports.getProblemList = (req, res) => {
   const { exam_id } = req.params;
   conn.query(
-    'SELECT * FROM Problem WHERE Problem.exam_id = ? Order by problem_num ASC',
-    [exam_id],
-    (err, result) => {
-      if (err) throw err;
-      return res.status(200).json({
-        result
-      })
-    }
+	'SELECT * FROM Problem WHERE Problem.exam_id = ? Order by problem_num ASC',
+	[exam_id],
+	(err, result) => {
+	  if (err) throw err;
+	  return res.status(200).json({
+		result
+	  })
+	}
   )
 }
 
 exports.updateProblem = (req, res) => {
   const { problem_num, small, activity, level, exam_id, accuracy, content, big, big_index, small_index } = req.body;
   conn.query(
-    'UPDATE Problem SET problem_num = ?, small = ?, activity = ?, level = ?, exam_id = ?, accuracy = ?, content = ?, big = ?, big_index = ?, small_index = ? WHERE exam_id = ? and problem_num = ?',
-    [problem_num, small, activity, level, exam_id, accuracy, content, big, big_index, small_index, exam_id, problem_num],
-    (err, result) => {
-      if (err) throw err;
-      return res.status(200).json({
-        message: 'success'
-      })
-    }
+	'UPDATE Problem SET problem_num = ?, small = ?, activity = ?, level = ?, exam_id = ?, accuracy = ?, content = ?, big = ?, big_index = ?, small_index = ? WHERE exam_id = ? and problem_num = ?',
+	[problem_num, small, activity, level, exam_id, accuracy, content, big, big_index, small_index, exam_id, problem_num],
+	(err, result) => {
+	  if (err) throw err;
+	  return res.status(200).json({
+		message: 'success'
+	  })
+	}
   )
 }
 
 exports.getOneExam = (req, res) => {
   const { exam_id } = req.params;
   conn.query(
-    'SELECT * FROM Exam WHERE id = ?',
-    [exam_id],
-    (err, result) => {
-      if (err) throw err;
-      return res.status(200).json({
-        result
-      })
-    }
+	'SELECT * FROM Exam WHERE id = ?',
+	[exam_id],
+	(err, result) => {
+	  if (err) throw err;
+	  return res.status(200).json({
+		result
+	  })
+	}
   )
 }
 
 exports.deleteExam = (req, res) => {
-    const { exam_id } = req.params;
-    conn.query(
-    'DELETE FROM Exam WHERE id = ?',
-    [exam_id],
-    (err, result) => {
-      if (err) throw err;
-      return res.status(200).json({
-        message: 'success'
-      })
-    }
-    )
+	const { exam_id } = req.params;
+	conn.query(
+	'DELETE FROM Exam WHERE id = ?',
+	[exam_id],
+	(err, result) => {
+	  if (err) throw err;
+	  return res.status(200).json({
+		message: 'success'
+	  })
+	}
+	)
 }
 
 exports.saveResult = (req, res) => {
-    const { name, grade, school, selectedId, selected } = req.body;
-    let answer = selected.split('');
-    // console.log(answer);
-    for (let i=0; i<answer.length; i++) {
-        conn.query(
-            'INSERT INTO Result(student_name, grade, test_id, school, result, problem_num) VALUES(?, ?, ?, ?, ?, ?)',
-            [name, grade, selectedId, school, answer[i], i+1],
-            (err) => {
-                if (err) throw err;
-            }
-        )
-    }
-    return res.status(200).json({
-        message: 'success'
-    })
+	const { name, grade, school, selectedId, selected } = req.body;
+	let answer = selected.split('');
+	// console.log(answer);
+	for (let i=0; i<answer.length; i++) {
+		conn.query(
+			'INSERT INTO Result(student_name, grade, test_id, school, result, problem_num) VALUES(?, ?, ?, ?, ?, ?)',
+			[name, grade, selectedId, school, answer[i], i+1],
+			(err) => {
+				if (err) throw err;
+			}
+		)
+	}
+	return res.status(200).json({
+		message: 'success'
+	})
 
 };
+
+
--- routes/api/exam/index.js
+++ routes/api/exam/index.js
@@ -2,11 +2,14 @@ const router = require('express').Router();
 const controller = require('./exam.controller');
 
 router.post('', controller.createExam);
+
+router.get('/result', controller.getResult);
 router.get('/all', controller.getExamList);
 router.get('/:exam_id', controller.getOneExam);
 router.delete('/:exam_id', controller.deleteExam);
 router.post('/problem', controller.createProblem);
 router.get('/problem/:exam_id', controller.getProblemList);
 router.put('/problem', controller.updateProblem);
+
 router.post('/result', controller.saveResult);
 module.exports = router;
